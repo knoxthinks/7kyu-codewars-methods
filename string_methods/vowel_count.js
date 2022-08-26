56a1ad568577cbf2a0f4d572fcd949f74d3253f5
Richard <itsknox@gmail.com>
2022-08-26 15:25:39 -0400

    answer

--- string_methods/vowel_count.js
+++ string_methods/vowel_count.js
@@ -18,3 +18,18 @@ function fooVowel(string){
   }
   fooVowel('richard')
   fooVowel('hired')
+
+  //answer
+//   function getCount(str) {
+//     var vowelsCount = 0;
+//     var vowels = ["a","e","i","o","u"];
+//     for(var i = 0;i < str.length;i++){
+//       for(var j=0;j<vowels.length;j++){
+//         if(str[i] === vowels[j]){
+//           vowelsCount++;
+//         }
+//       }
+//     }
+    
+//     return vowelsCount;
+//   }
