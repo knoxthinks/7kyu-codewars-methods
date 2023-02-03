5418c200d7d6e2491b389f5a7da25fb2378364b2
knoxLaptop <itsknox@gmail.com>
2023-02-03 11:27:20 -0500

    split in es6 format

---
+++ z. Practice/stringToArr.js
@@ -0,0 +1,9 @@
+//writing a function to split a string
+//convert it to and arr of words
+
+function stringToArr(string){
+    return string.split(' ')
+}
+
+//write it in es6
+const stringToArr = (s) => s.split(' ')
